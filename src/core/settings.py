553676a404e07866b5e7b94dd281ea3836bc22ca
Maks3w <github.maks3w@virtualplanets.net>
2021-01-29 13:50:08 +0100

    [project] Add BasicAuthentication config
    Allow to use Basic Auth when required

--- src/core/settings.py
+++ src/core/settings.py
@@ -39,6 +39,9 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
