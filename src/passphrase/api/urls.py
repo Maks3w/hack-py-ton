f1c075ce4bdaa6848a31441d1b10375c118fb36d
Maks3w <github.maks3w@virtualplanets.net>
2021-01-30 12:14:43 +0100

    [passphrase] Fix route should not have leading "/"

--- src/passphrase/api/urls.py
+++ src/passphrase/api/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'passphrase_api'
 urlpatterns = [
-    path('/basic', views.BasicValidationView.as_view(), name='basic_validation'),
-    path('/advanced', views.AdvancedValidationView.as_view(), name='advanced_validation'),
+    path('basic', views.BasicValidationView.as_view(), name='basic_validation'),
+    path('advanced', views.AdvancedValidationView.as_view(), name='advanced_validation'),
 ]
